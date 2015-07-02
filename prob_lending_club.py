b38c45ef928c8a18407567c9ba2092f22cd0ba4b
ugp <ugp@ugps-MacBook-3.local>
2015-07-01 19:37:50 -0500

    pushign up first draft

---
+++ prob_lending_club.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+#import for QQ plots?
+import scipy.stats as stats
+
+#Import as Pandas Dataframe on this line?: 
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+
+#Remove rows will null values: 
+loansData.dropna(inplace=True)
+
+#Generate a Box Plot: 
+loansData.boxplot(column='Amount.Requested')
+return_type ='dict'
+plt.show()
+
+# Generate a Histogram: 
+loansData.hist(column='Amount.Requested')
+plt.show()
+
+# Generate QQ Plot: 
+plt.figure()
+graph = stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
+plt.show()
+
+
