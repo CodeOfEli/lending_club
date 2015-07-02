3a7f22f3a8d274ec76574cbbd82ba45101e1d835
ugp <ugp@ugps-MacBook-3.local>
2015-07-01 19:46:00 -0500

    Not sure how to compare two different columns

--- prob_lending_club.py
+++ prob_lending_club.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 #import for QQ plots?
 import scipy.stats as stats
+import collections
 
 #Import as Pandas Dataframe on this line?: 
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
@@ -9,18 +10,27 @@ loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loan
 #Remove rows will null values: 
 loansData.dropna(inplace=True)
 
+#Do Collectinons
+# c = collections.Counter(loansData['Amount.Funded.By.Investors'])
+# print c
+
+
 #Generate a Box Plot: 
 loansData.boxplot(column='Amount.Requested')
+loansData.boxplot(column='Amount.Funded.By.Investors')
 return_type ='dict'
 plt.show()
 
 # Generate a Histogram: 
 loansData.hist(column='Amount.Requested')
+loansData.hist(column='Amount.Funded.By.Investors')
+
 plt.show()
 
 # Generate QQ Plot: 
 plt.figure()
 graph = stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
+graph = stats.probplot(loansData['Amount.Funded.By.Investors'], dist="norm", plot=plt)
 plt.show()
 
 
